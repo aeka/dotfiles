7d1443df05cee24b4c89b755bdf5de516a7bf257
aeka <rekiws3@gmail.com>
2018-12-07 11:52:44 +0800

    startup from powershell to powershell core

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,7 +5,29 @@ function prompt
    return " "
 }
 
-set-location $home 
+function Get-Batchfile ($file) {
+    $cmd = "`"$file`" & set"
+    cmd /c $cmd | Foreach-Object {
+        $p, $v = $_.split('=')
+        Set-Item -path env:$p -value $v
+    }
+}
+
+function VS17()
+{
+    $vs140comntools = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\"
+    $batchFile = [System.IO.Path]::Combine($vs140comntools, "VsDevCmd.bat")
+    Get-Batchfile $BatchFile
+
+    [System.Console]::Title = "Visual Studio 2017 Windows PowerShell"
+}
+
+# custom shortcuts
+function ncadir { cd C:\Projects\NCA\ }
+function kafkadir { cd C:\Kafka\kafka_2.11-2.0.0\bin\windows }
+function zkdir { cd C:\Kafka }
+
+VS17
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
